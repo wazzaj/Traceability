860dcc1df52e86c41d9f076af4054b5224ed309a
Warren Jones <Warren Jones>
2016-05-24 22:25:56 +1000

    Use SDK

--- Import.py
+++ Import.py
@@ -1,49 +1,53 @@
-import urllib2
-import json
-import base64
 import pyral
-#import requests
+import requests
+from pyral import Rally, rallyWorkset
+import sys
 
-username = 'xxx@xxx.com'
-password = 'xxxxxxxx'
+username = 'dan@acme.com'
+password = 'Motivate!'
 host = 'https://demo-apac.rallydev.com/slm/webservice/v2.0/'
 
 def setUpBasicAuth(r):
-    base64string = base64.b64encode(bytes('%s:%s' % (username, password),'ascii'))
-    r.add_header("Authorization", "Basic %s" % base64string.decode('utf-8'))
+
     return()
 
-def addDependenciesToRally(Pre, Post):
-    request = urllib.request.Request(host +
-            'hierarchicalrequirement?query=(FormattedID%20%3D%20' + Pre + ')' +
-            '&fetch=true')
+def addDependenciesToRally(rally, pre, Post):
+    response = rally.get('UserStory', fetch=True, query="FormattedID = %s" % pre)
 
-    print(host +
-            'hierarchicalrequirement?query=(FormattedID%20%3D%20' + Pre + ')' +
-            '&fetch=true')
-    
-    setUpBasicAuth(request)
-    result = urllib.request.urlopen(request).read()
+    print response.resultCount
+#    if not response.errors:
+    for story in response:
+        print story
+        print("Match on " + story.FormattedID)
+        print story.details()
+#        else:
+#            print("Errors" + str(response.errors))
 
-    jsonResponse = json.loads(result.decode('utf-8'))
-#    print(json.dumps(jsonResponse))
-
-    if jsonResponse["QueryResult"]["TotalResultCount"] > 0:
-        print('Result ' + str(jsonResponse["QueryResult"]["TotalResultCount"]))
-        
     return()
 
-def getDependencies(host):
+def getDependencies(rally):
     import pyexcel as pe
     import pyexcel.ext.xlsx
 
     records = pe.get_records(file_name="Test.xlsx")
     for record in records:
         print(record['Pre'],record['Post'])
-        addDependenciesToRally(record['Pre'],record['Post'])
+        addDependenciesToRally(rally,record['Pre'],record['Post'])
           
-    return()                              
-          
-getDependencies(host)
+    return()
+
+
+def setupRally():
+    options = [arg for arg in sys.argv[1:] if arg.startswith('--')]
+    args = [arg for arg in sys.argv[1:] if arg not in options]
+    server, user, password, apikey, workspace, project = rallyWorkset(options)
+    print("S: " + str(server) + " U: " + user + " P: " + password + " A: " + apikey + " W: " + workspace)
+    rally = Rally(server, user, password, workspace=workspace, project=project)
+    rally.enableLogging('mypyral.log')
+
+    return(rally)
+
+myRally = setupRally()
+getDependencies(myRally)
 
 exit()

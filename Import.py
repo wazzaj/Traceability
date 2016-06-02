d043ab4e77e5aca95e0ed293377f6a4ab7e0ee6c
Warren Jones <Warren Jones>
2016-06-02 22:16:37 +1000

    Changed to work for Portfolio Items, not Stories

--- Import.py
+++ Import.py
@@ -9,11 +9,11 @@ errout = sys.stderr.write
 
 ####################################################################################################
 
-def addDependenciesToRally(rally, pre, post):
-    postStory = getStory(rally, pre)
-    print "S: " + postStory.FormattedID
-    predecessor = getStory(rally, post)
+def addDependenciesToRally(rally, story, post):
+    predecessor = getItem(rally, story)
     print "P: " + predecessor.FormattedID
+    postStory = getItem(rally, post)
+    print "S: " + postStory.FormattedID
 
     newList = list()
 
@@ -25,6 +25,9 @@ def addDependenciesToRally(rally, pre, post):
     newList.append({"_ref" : str(predecessor.ref)})
 
     info = dict(FormattedID=postStory.FormattedID, Predecessors=newList)
+    print info
+    return()
+
     print "Issuing POST request..."
 
     try:
@@ -34,19 +37,22 @@ def addDependenciesToRally(rally, pre, post):
         sys.stderr.write('ERROR: %s \n' % details)
         sys.exit(2)
 
-    print "Story Updated"
+    print "Item Updated"
     print "FormattedID: %s Predecessor: %s" % (updatedStory.FormattedID, predecessor.FormattedID)
 
     return ()
 
-def getStory(rally, storyID):
+def getItem(rally, id):
     # type: (object, object) -> object
     print "Processing GET request..."
-    response = rally.get('UserStory', fetch="_ref,ObjectID,FormattedID,Name,Predecessors", query="FormattedID = %s" % storyID)
+    response = rally.get('PortfolioItem', fetch="_ref,ObjectID,FormattedID,Name,Predecessors", query="FormattedID = %s" % id)
 
     if not response.errors:
         for story in response:
             continue
+    else:
+        print response.errors
+        sys.exit(9)
 
     return (story)
 
@@ -54,9 +60,10 @@ def getDependencies(rally):
     import pyexcel as pe
     import pyexcel.ext.xlsx
 
-    records = pe.get_records(file_name="Test.xlsx")
+    records = pe.get_records(file_name="RallyUpload-DependenciestoCA-v2.xlsx")
     for record in records:
-        addDependenciesToRally(rally, record['Pre'], record['Post'])
+        print record['RALLY ID'] + " - " + record['Successor']
+        addDependenciesToRally(rally, record['RALLY ID'], record['Successor'])
 
     return ()
 
@@ -65,8 +72,12 @@ def setupRally():
     options = [arg for arg in sys.argv[1:] if arg.startswith('--')]
     args = [arg for arg in sys.argv[1:] if arg not in options]
     server, user, password, apikey, workspace, project = rallyWorkset(options)
-    print("S: " + str(server) + " U: " + user + " P: " + password + " A: " + apikey + " W: " + workspace)
-    rally = Rally(server, user, password, workspace=workspace, project=project)
+    if apikey:
+        print("S: " + str(server) + " A: " + apikey + " W: " + workspace + " P: " + project)
+        rally = Rally(server, apikey=apikey, workspace=workspace, project=project)
+    else:
+        print("S: " + str(server) + " U: " + user + " P: " + password + " W: " + workspace + " P: " + project)
+        rally = Rally(server, user, password, workspace=workspace, project=project)
     rally.enableLogging('mypyral.log')
 
     return (rally)
